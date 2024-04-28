5d97e797e60a2652bf07fbd03e53ad36dd7ab8ef
Arkye20 <164972616+Arkye20@users.noreply.github.com>
2024-04-28 19:37:41 +0800

    Update detect.py
    将run()拆分成两个函数，删除了部分代码。

--- yolov3/detect.py
+++ yolov3/detect.py
@@ -35,6 +35,125 @@ from utils.plots import Annotator, colors, save_one_box
 from utils.torch_utils import select_device, time_sync
 from models.experimental import attempt_load
 
+# 将run()函数拆分成prepare_model()和predict_image()
+# 加载模型
+def prepare_model(
+        weights = './yolov3/weights/yolov3.pt', # model.pt path(s)
+        device='cpu', # cuda device, i.e. 0 or 0,1,2,3 or cpu
+):
+    # Load model
+    device = select_device(device)
+    model = DetectMultiBackend(weights, device=device, dnn=False)
+    return model
+
+# 预测图片 删除原函数的一些功能
+def predict_image(
+        source='./data/images/bb',
+        imgsz=640,
+        model=None,
+        conf_thres=0.25,
+        iou_thres=0.25,
+        max_det=1000,
+        device='cpu',
+        save_crop=False,
+        classes=None,
+        project=ROOT / 'runs/detect',
+        name='gradio_exp',
+        exist_ok=True,
+        line_thickness=1,
+        hide_labels=False,
+        hide_conf=False,
+        SAVE_DIR=None
+):
+    stride, names, pt, jit, onnx = model.stride, model.names, model.pt, model.jit, model.onnx
+    imgsz = check_img_size(imgsz, s=stride)  # check image size
+
+    # Directories
+    if SAVE_DIR is None:
+        save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
+        save_dir.mkdir(parents=True, exist_ok=True)  # make dir
+    else:
+        save_dir = SAVE_DIR
+
+    # File Type Process
+    source = str(source)
+    save_img = True  # save inference images
+    is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
+    is_url = source.lower().startswith(('rtsp://', 'rtmp://', 'http://', 'https://'))
+    if is_url and is_file:
+        source = check_file(source)  # download
+
+    # Dataloader
+    dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt and not jit)
+
+    # Run inference
+    dt, seen = [0.0, 0.0, 0.0], 0
+    for path, im, im0s, vid_cap, s in dataset:
+        t1 = time_sync()
+        im = torch.from_numpy(im).to(device)
+        im = im.float()  # uint8 to fp16/32
+        im /= 255  # 0 - 255 to 0.0 - 1.0
+        if len(im.shape) == 3:
+            im = im[None]  # expand for batch dim
+        t2 = time_sync()
+        dt[0] += t2 - t1
+
+        # Inference
+        pred = model(im, augment=False, visualize=False)
+        t3 = time_sync()
+        dt[1] += t3 - t2
+
+        # NMS
+        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, False, max_det=max_det)
+        dt[2] += time_sync() - t3
+
+        # Process predictions
+        for i, det in enumerate(pred):  # per image
+            seen += 1
+            p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
+
+            p = Path(p)  # to Path
+            save_path = os.path.join(save_dir, p.name)
+            s += '%gx%g ' % im.shape[2:]  # print string
+            imc = im0.copy() if save_crop else im0  # for save_crop
+            annotator = Annotator(im0, line_width=line_thickness, example=str(names))
+            if len(det):
+                if type(det) is list:
+                    det = det[0]
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    if save_img or save_crop:  # Add bbox to image
+                        c = int(cls)  # integer class
+                        label = None if hide_labels else (names[c] if hide_conf else f'{names[c]} {conf:.2f}')
+                        annotator.box_label(xyxy, label, color=colors(c, True))
+                        if save_crop:
+                            save_one_box(xyxy, imc, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
+
+            # Print time (inference-only)
+            LOGGER.info(f'{s}Done. ({t3 - t2:.3f}s)')
+
+            # Stream results
+            im0 = annotator.result()
+
+            # Save results (image with detections)
+            if save_img:
+                if dataset.mode == 'image':
+                    cv2.imwrite(save_path, im0)
+            break
+
+    if save_img:
+        s = ''
+        LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
+    return save_dir
+
 @torch.no_grad()
 def run(weights='./weights/yolov3.pt',  # model.pt path(s)
         source= './output_adv/',  # file/dir/URL/glob, 0 for webcam
@@ -265,5 +384,15 @@ def main(opt):
 
 
 if __name__ == "__main__":
-    opt = parse_opt()
-    main(opt)
+    # opt = parse_opt()
+    # main(opt)
+
+    weights = './weights/yolov3.pt'  # model.pt path(s)
+    model = prepare_model(weights=weights)
+
+    IMAGES_PATH = '../gradio/images3/'
+    SAVE_DIR = '../gradio/predicted_images/'
+    images = [os.path.join(IMAGES_PATH, file) for file in os.listdir(IMAGES_PATH)]
+    for image in images:
+        predict_image(source=image, model=model, SAVE_DIR=SAVE_DIR)
+
